093ed5155a6276cfdf25101cdbf9219374ed585a
ridesun <ridesungg@gmail.com>
2025-02-09 12:48:44 +0800

    finish

---
+++ exercises/algorithm/algorithm9.rs
@@ -0,0 +1,193 @@
+/*
+	heap
+	This question requires you to implement a binary heap function
+*/
+
+
+use std::cmp::Ord;
+use std::default::Default;
+
+pub struct Heap<T>
+where
+    T: Default,
+{
+    count: usize,
+    items: Vec<T>,
+    comparator: fn(&T, &T) -> bool,
+}
+
+impl<T> Heap<T>
+where
+    T: Default,
+{
+    pub fn new(comparator: fn(&T, &T) -> bool) -> Self {
+        Self {
+            count: 0,
+            items: vec![T::default()],
+            comparator,
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.count
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    pub fn add(&mut self, value: T) {
+        self.count += 1;
+        if self.count >= self.items.len() {
+            self.items.push(value);
+        } else {
+            self.items[self.count] = value;
+        }
+        let mut idx = self.count;
+        while idx > 1 {
+            let parent = self.parent_idx(idx);
+            if (self.comparator)(&self.items[idx], &self.items[parent]) {
+                self.items.swap(idx, parent);
+                idx = parent;
+            } else {
+                break;
+            }
+        }
+    }
+
+    fn parent_idx(&self, idx: usize) -> usize {
+        idx / 2
+    }
+
+    fn children_present(&self, idx: usize) -> bool {
+        self.left_child_idx(idx) <= self.count
+    }
+
+    fn left_child_idx(&self, idx: usize) -> usize {
+        idx * 2
+    }
+
+    fn right_child_idx(&self, idx: usize) -> usize {
+        self.left_child_idx(idx) + 1
+    }
+
+    fn smallest_child_idx(&self, idx: usize) -> usize {
+        let left = self.left_child_idx(idx);
+        let right = self.right_child_idx(idx);
+        if right <= self.count &&(self.comparator)(&self.items[right], &self.items[left]) {
+            right
+        } else {
+            left
+        }
+    }
+}
+
+impl<T> Heap<T>
+where
+    T: Default + Ord,
+{
+    /// Create a new MinHeap
+    pub fn new_min() -> Self {
+        Self::new(|a, b| a < b)
+    }
+
+    /// Create a new MaxHeap
+    pub fn new_max() -> Self {
+        Self::new(|a, b| a > b)
+    }
+}
+
+impl<T> Iterator for Heap<T>
+where
+    T: Default+Clone,
+{
+    type Item = T;
+
+    fn next(&mut self) -> Option<T> {
+        if self.is_empty() {
+            None
+        } else {
+            let result = self.items[1].clone();
+            self.items[1] = self.items[self.count].clone();
+            self.count -= 1;
+
+            if !self.is_empty() {
+                let mut idx = 1;
+                while self.children_present(idx) {
+                    let child = self.smallest_child_idx(idx);
+                    if (self.comparator)(&self.items[child], &self.items[idx]) {
+                        self.items.swap(child, idx);
+                        idx = child;
+                    } else {
+                        break;
+                    }
+                }
+            }
+            Some(result)
+        }
+    }
+}
+
+pub struct MinHeap;
+
+impl MinHeap {
+    #[allow(clippy::new_ret_no_self)]
+    pub fn new<T>() -> Heap<T>
+    where
+        T: Default + Ord,
+    {
+        Heap::new(|a, b| a < b)
+    }
+}
+
+pub struct MaxHeap;
+
+impl MaxHeap {
+    #[allow(clippy::new_ret_no_self)]
+    pub fn new<T>() -> Heap<T>
+    where
+        T: Default + Ord,
+    {
+        Heap::new(|a, b| a > b)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_empty_heap() {
+        let mut heap = MaxHeap::new::<i32>();
+        assert_eq!(heap.next(), None);
+    }
+
+    #[test]
+    fn test_min_heap() {
+        let mut heap = MinHeap::new();
+        heap.add(4);
+        heap.add(2);
+        heap.add(9);
+        heap.add(11);
+        assert_eq!(heap.len(), 4);
+        assert_eq!(heap.next(), Some(2));
+        assert_eq!(heap.next(), Some(4));
+        assert_eq!(heap.next(), Some(9));
+        heap.add(1);
+        assert_eq!(heap.next(), Some(1));
+    }
+
+    #[test]
+    fn test_max_heap() {
+        let mut heap = MaxHeap::new();
+        heap.add(4);
+        heap.add(2);
+        heap.add(9);
+        heap.add(11);
+        assert_eq!(heap.len(), 4);
+        assert_eq!(heap.next(), Some(11));
+        assert_eq!(heap.next(), Some(9));
+        assert_eq!(heap.next(), Some(4));
+        heap.add(1);
+        assert_eq!(heap.next(), Some(2));
+    }
+}
